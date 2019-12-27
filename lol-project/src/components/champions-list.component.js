af6488133c467c5c1a3e83f1cf89318b23e9fc9a
Steven Tan <steventangerine@gmail.com>
2019-12-26 22:30:29 -0800

    Nav Bar Started

---
+++ lol-project/src/components/champions-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ChampionsList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Select A Champion</p>
+            </div>
+        )
+    }
+}
---
+++ lol-project/src/components/search-player.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SearchPlayer extends Component {
+    render() {
+        return (
+            <div>
+                <p>Search Players</p>
+            </div>
+        )
+    }
+}

1783dbff89814a55408f7fea510d88b66dcf4cd9
Steven Tan <steventangerine@gmail.com>
2019-12-29 05:00:11 -0800

    Champ List edit

--- lol-project/src/components/champions-list.component.js
+++ lol-project/src/components/champions-list.component.js
@@ -17,169 +17,62 @@ import Blitzcrank from "../assets/icons/B/Blitzcrank-Icon.png"
 import Brand from "../assets/icons/B/Brand-Icon.png"
 import Braum from "../assets/icons/B/Braum-Icon.png"
 
+import styled from "styled-components"
+const Styles = styled.div`
+
+.img-responsive{
+    padding: 1px 1px;
+}
+
+`;
 
 
 export const ChampionsList = () => (
-    <html lang="en">
-    <header><h1>Choose a Champion</h1></header>
+    <Styles>
+        <div class="container-fluid">
 
-    <div class ="container">
-        <div class = "row">
-            <h1>A</h1>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Aatrox"><img class="img-responsie" src={Aatrox}></img></a>
-                
+            <header><h1>Choose a Champion</h1></header>
+            <div class="form-group mb-4">
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
             </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Ahri"><img class="img-responsie" src={Ahri}></img></a>
-                
+            
+
+                <div class="row">
+                    <a href="/Aatrox"><img class="img-responsive" src={Aatrox}></img></a>
+                    <a href="/Ahri"><img class="img-responsive" src={Ahri}></img></a>
+
+                    <a href="/Akali"><img class="img-responsive" src={Akali}></img></a>
+
+
+                    <a href="/Alistar"><img class="img-responsive" src={Alistar}></img></a>
+
+
+                    <a href="/Amumu"><img class="img-responsive" src={Amumu}></img></a>
+
+
+                    <a href="/Anivia"><img class="img-responsive" src={Anivia}></img></a>
+
+                    <a href="/Annie"><img class="img-responsive" src={Annie}></img></a>
+
+                    <a href="/Aphelios"><img class="img-responsive" src={Aphelios}></img></a>
+
+                    <a href="/Ashe"><img class="img-responsive" src={Ashe}></img></a>
+
+                    <a href="/AurelionSol"><img class="img-responsive" src={AurelionSol}></img></a>
+
+                    <a href="/Azir"><img class="img-responsive" src={Azir}></img></a>
+
+                    <a href="/Bard"><img class="img-responsive" src={Bard}></img></a>
+
+                    <a href="/Blitzcrank"><img class="img-responsive" src={Blitzcrank}></img></a>
+
+                    <a href="/Brand"><img class="img-responsive" src={Brand}></img></a>
+
+                    <a href="/Braum"><img class="img-responsive" src={Braum}></img></a>
+                </div>
             </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Akali"><img class="img-responsie" src={Akali}></img></a>
-                
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Alistar"><img class="img-responsie" src={Alistar}></img></a>
-                
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Amumu"><img class="img-responsie" src={Amumu}></img></a>
-                
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Anivia"><img class="img-responsie" src={Anivia}></img></a>
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Annie"><img class="img-responsie" src={Annie}></img></a>
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Aphelios"><img class="img-responsie" src={Aphelios}></img></a>
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Ashe"><img class="img-responsie" src={Ashe}></img></a>
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/AurelionSol"><img class="img-responsie" src={AurelionSol}></img></a>
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Azir"><img class="img-responsie" src={Azir}></img></a>
-            </div>
-        </div>
 
-        <div class="row">
-            <h1>B</h1>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Bard"><img class="img-responsie" src={Bard}></img></a>
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Blitzcrank"><img class="img-responsie" src={Blitzcrank}></img></a>
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Brand"><img class="img-responsie" src={Brand}></img></a>
-            </div>
-            <div class = "col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                <a href="/Braum"><img class="img-responsie" src={Braum}></img></a>
-            </div>
-        </div>
-
-        <div class="row">
-            <h1>C</h1>
-        </div>
-
-        <div class="row">
-            <h1>D</h1>
-        </div>
-
-        <div class="row">
-            <h1>E</h1> 
-        </div>
-
-        <div class="row">
-            <h1>F</h1>
-        </div>
-
-        <div class="row">
-            <h1>G</h1>
-        </div>
-
-        <div class="row">
-            <h1>H</h1>
-        </div>
-
-        <div class="row">
-            <h1>I</h1>
-        </div>
-
-        <div class="row">
-            <h1>J</h1>
-        </div>
-
-        <div class="row">
-            <h1>K</h1>
-        </div>
-
-        <div class="row">
-            <h1>L</h1>
-        </div>
-
-        <div class="row">
-            <h1>M</h1>
-        </div>
-
-        <div class="row">
-            <h1>N</h1>
-        </div>
-
-        <div class="row">
-            <h1>O</h1>
-        </div>
-
-        <div class="row">
-            <h1>P</h1>
-        </div>
-
-        <div class="row">
-            <h1>Q</h1>
-        </div>
-
-        <div class="row">
-            <h1>R</h1>
-        </div>
-
-        <div class="row">
-            <h1>S</h1>
-        </div>
-
-        <div class="row">
-            <h1>T</h1>
-        </div>
-
-        <div class="row">
-            <h1>U</h1>
-        </div>
-
-        <div class="row">
-            <h1>V</h1>
-        </div>
-
-        <div class="row">
-            <h1>W</h1>
-        </div>
-
-        <div class="row">
-            <h1>X</h1>
-        </div>
-
-        <div class="row">
-            <h1>Y</h1>
-        </div>
-
-        <div class="row">
-            <h1>Z</h1>
-        </div>
-    </div>
-
-
-    </html>
+            
+    </Styles>
 )
 
--- lol-project/src/components/navbar.component.js
+++ lol-project/src/components/navbar.component.js
@@ -19,7 +19,7 @@ const Styles = styled.div`
 export const NavigationBar = () => (
     <Styles>
         <Navbar expand="lg">
-            <Navbar.Brand href="/">LoL Project</Navbar.Brand>
+            <Navbar.Brand href="/">GG.OP</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
